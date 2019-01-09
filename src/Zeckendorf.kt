67b54b9eaff0c3973e61d052ac793a0baf3b76b7
0xflotus <0xflotus@gmail.com>
2019-01-09 01:35:10 +0100

    initial commit

---
+++ src/Zeckendorf.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+    println(zeckendorf(args.get(0).toInt()))
+}
+
+fun zeckendorf(m: Int): String {
+    var n = m
+    if (n == 0) {
+        return "0"
+    }
+
+    val fibs = mutableListOf(1)
+    var next = 2
+    while (next <= n) {
+        fibs.add(next)
+        next += fibs.get(fibs.size - 2)
+    }
+
+    var sb = ""
+    fibs.reverse()
+    fibs.forEach {
+        sb += if (it <= n) "1" else "0"
+        if (it <= n) {
+            n -= it
+        }
+    }
+    return sb
+}
